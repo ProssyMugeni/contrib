afcdd2c00e94c2782e5fe64dec550f2e2cfa1140
ProssyMugeni <mugeniprossy53@gmail.com>
2019-05-10 14:06:22 +0300

    set up

---
+++ footer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './components/Footer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
